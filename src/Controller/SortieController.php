c3ab55c4351eedef74f64f6fdfc3a946b3df5890
Mathilde-Bonard <mathildebonard@gmail.com>
2025-09-03 11:28:06 +0200

    SortieController

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,13 +2,12 @@
 
 namespace App\Controller;
 
-<<<<<<< HEAD
+
 use App\Entity\Sortie;
 use App\Form\SortieCreationType;
-=======
 use App\Form\FilterType;
 use App\Repository\SortieRepository;
->>>>>>> origin/Max
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,20 +15,6 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class SortieController extends AbstractController
 {
-<<<<<<< HEAD
-    #[Route('/add', name: 'sortie_add')]
-    public function add(
-        Request $request
-    ): Response
-    {
-        $sortie = new Sortie();
-        $sortieFormCreation = $this->createForm(SortieCreationType::class, $sortie);
-
-        $sortieFormCreation->handleRequest($request);
-
-        return $this->render('sortie/create.html.twig', [
-            'sortieFormCreation' => $sortieFormCreation
-=======
     #[Route('/', name: 'app_sortie')]
     #[Route('/accueil', name: 'app_sortie_2')]
     public function index(SortieRepository $sortieRepository): Response
@@ -42,8 +27,23 @@ final class SortieController extends AbstractController
         return $this->render('sortie/index.html.twig', [
             'filterForm' => $filterForm->createView(),
             'sorties' => $sorties,
->>>>>>> origin/Max
+
         ]);
     }
+    #[Route('/add', name: 'sortie_add')]
+    public function add(
+        Request $request
+    ): Response
+    {
+        $sortie = new Sortie();
+        $sortieFormCreation = $this->createForm(SortieCreationType::class, $sortie);
 
+        $sortieFormCreation->handleRequest($request);
+
+        return $this->render('sortie/create.html.twig', [
+            'sortieFormCreation' => $sortieFormCreation
+            }
 }
+
+
+
